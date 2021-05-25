e77f737c5ebbe29e1c9f6718406b53162f1a84ea
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-05-25 15:11:48 +0000

    フォローフォロワー一覧

--- app/helpers/user/notifications_helper.rb
+++ app/helpers/user/notifications_helper.rb
@@ -7,17 +7,15 @@ module User::NotificationsHelper
     #notification.actionがfollowかgoodかcommentかchatか
     case notification.action
       when "follow" then
-        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしましたああああ"
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
       when "chat" then
-        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"xxxあああ"
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"があなたにチャットを送信しました"
         #tag.a(notification.chat.user.name, href:user_path(notification.chat.user), style:"font-weight: bold;")+"があなたにメッセージを送信しました"
       when "good" then
         tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にいいねしました"
       when "comment" then
           @comment = Comment.find_by(id: @visiter_comment)&.comments_body
           tag.a(@visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にコメントしました"
-      else
-         tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"xx"
     end
   end
 
